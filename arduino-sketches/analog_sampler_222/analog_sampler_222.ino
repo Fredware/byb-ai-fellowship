f6fbbdf255c34701c26085b6b4c34321f519def7
Fredware <35937021+Fredware@users.noreply.github.com>
2021-07-27 15:41:11 -0400

    Refactor update
    Removed dead code

--- arduino-sketches/analog_sampler_222/analog_sampler_222.ino
+++ arduino-sketches/analog_sampler_222/analog_sampler_222.ino
@@ -25,8 +25,8 @@
 #define CH_03 16
 #define CH_04 17
 #define CH_05 18
-/* ------------------------------------------------------------------------- */
 
+/* ------------------------------------------------------------------------- */
 #define N_CHANS 5
 #define FRAME_LEN 20
 #define FEATURE_LEN 11
@@ -49,23 +49,14 @@
 #define SRV_CLOSE 0 // angle for closed finger
 #define SRV_OPEN 180 // angle for open finger
 
-#define BUFFER_SIZE 66
-
-int finalReading1;
-int finalReading2;
-int finalReading3;
-int finalReading4;
-
-int servoPins[SERVOS] = {9,10,11,12,13}; // servos on pins 9 through 13 (thumb, index, middle, ring, pinky)
-int servoMin[SERVOS] = {1000,544,544,544,544}; // minimum pwm value for each servo
-int servoMax[SERVOS] = {1750 ,2400,2400,2400,2400}; // maximum pwm value for each servo
-int servoInv[SERVOS] = {0,0,0,0,0}; // indicates if servo direction is inverted
+int servoPins[SERVOS] = {9, 10, 11, 12, 13}; // servos on pins 9 through 13 (thumb, index, middle, ring, pinky)
+int servoMin[SERVOS] = {1000, 544, 544, 544, 544}; // minimum pwm value for each servo
+int servoMax[SERVOS] = {1750, 2400, 2400, 2400, 2400}; // maximum pwm value for each servo
+int servoInv[SERVOS] = {0, 0, 0, 0, 0}; // indicates if servo direction is inverted
 
 unsigned long tmsServo_Attached[SERVOS]; // time since last servo attach. Used to reduce power comsumption
-unsigned long previousTime = 0;
 
 Servo myservo[SERVOS];
-/* ------------------------------------------------------------------------- */
 
 /* Dataset Statistics ------------------------------------------------------ */
 float means[5] = {  -14981.99126183,
@@ -90,34 +81,18 @@ float norms[5] = {  19.3109078,
                     };
 
 float alpha[5];
-/* ------------------------------------------------------------------------- */
 
 /* 200Hz Analog Sampler ---------------------------------------------------- */
-//auto sampling_timer = timer_create_default();
-//int debug_counter = 0;
 uint32_t temp_data;
-/* ------------------------------------------------------------------------- */
 
 /* Inference Model --------------------------------------------------------- */
-// static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
-//float features[] = {
-//    // copy raw features here (for example from the 'Live classification' page)
-//    // see https://docs.edgeimpulse.com/docs/running-your-impulse-arduino
-//    -0.3809, -0.4746, -0.5160, -0.5489, -0.8806, -0.3134, -0.4810, -0.0593, -0.5490, -0.7550, -0.2870,
-//    -0.3899, 0.4217, -0.5493, -0.8335, -0.2843, -0.4189, 1.9378, -0.4997, -1.0188, -0.3034, -0.6140,
-//    3.7326, -0.2669, -1.0113, -0.1515, -0.5053, 4.4153, 0.0351, -0.4798, 0.1105, -0.4794, 5.5325,
-//    2.6580, 0.1328, 0.2387, -0.3932, 5.7938, 4.0086, 0.3559, 0.4816, -0.0272, 6.1214, 4.4982, 
-//    1.1336, 1.0228, 0.1696, 5.7815, 3.3322, 1.6830, 1.4567, 0.4759, 5.5055, 2.2049, 2.8162
-//};
-/* ------------------------------------------------------------------------- */
 float features [55] = {0};
 
-/* Filtering --------------------------------------------------------------- */
-//filter formula can be found here:
-// https://www.earlevel.com/main/2003/02/28/biquads/
-// y[n] = a0*x[n] + a1*x[n-1] + a2*x[n-2] – b1*y[n-1] – b2*y[n-2]
-//b0, b1, b2, a1, a2 can be calculated here:
-//https://www.earlevel.com/main/2013/10/13/biquad-calculator-v2/
+/* Filtering --------------------------------------------------------------- 
+ * filter formula: https://www.earlevel.com/main/2003/02/28/biquads/
+ *                 y[n] = a0*x[n] + a1*x[n-1] + a2*x[n-2] – b1*y[n-1] – b2*y[n-2]
+ * coefficient calculator: https://www.earlevel.com/main/2013/10/13/biquad-calculator-v2/
+*/
 float a0 = 0.010995248654511658;
 float a1 = 0.021990497309023315;
 float a2 = 0.010995248654511658;
@@ -201,7 +176,6 @@ void filterData03(float newValue[])
   }
   return;
 }
-/* ------------------------------------------------------------------------- */
 
 /* Inference Model --------------------------------------------------------- */
 int raw_feature_get_data(size_t offset, size_t length, float *out_ptr) {
@@ -209,38 +183,25 @@ int raw_feature_get_data(size_t offset, size_t length, float *out_ptr) {
     return 0;
 }
 
-void ei_printf(const char *format, ...) 
-{
-   static char print_buf[1024] = { 0 };
-
-   va_list args;
-   va_start(args, format);
-   int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
-   va_end(args);
-
-   if (r > 0) 
-   {
-       Serial.write(print_buf);
-   }
-}
-/* ------------------------------------------------------------------------- */
-
-
 /* Motor Control ----------------------------------------------------------- */
 void moveFinger(int finger, int pos)
 {
-  if (pos == 1) { // Close
-    if (myservo[finger].read() != abs(SRV_CLOSE - SRV_OPEN*servoInv[finger])) {
-      // Close finger
-      if (!myservo[finger].attached()) {
+  if (pos == 1) 
+  { // Close
+    if (myservo[finger].read() != abs(SRV_CLOSE - SRV_OPEN*servoInv[finger]))
+    {// Close finger
+      if (!myservo[finger].attached())
+      {
         myservo[finger].attach(servoPins[finger], servoMin[finger], servoMax[finger]);
         tmsServo_Attached[finger] = millis();
       }
       myservo[finger].write(abs(SRV_CLOSE - SRV_OPEN*servoInv[finger]));
     }
   }
-  else { // Open
-    if (myservo[finger].read() != abs(SRV_OPEN - SRV_OPEN*servoInv[finger])) {
+  else 
+  { // Open
+    if (myservo[finger].read() != abs(SRV_OPEN - SRV_OPEN*servoInv[finger]))
+    {
       // Open finger
       if (!myservo[finger].attached()) 
       {
@@ -263,16 +224,10 @@ void execute_finger_sequence( int finger_idx)
   delay(period_ms);
   return;
 }
-/* ------------------------------------------------------------------------- */
 
-/*****************************************************************************/
-/*****************************************************************************/
-/************************************ SETUP **********************************/
-/*****************************************************************************/
-/*****************************************************************************/
-#define SAMPLING_PIN 3
-#define DELAY_PIN 4
-
+/*****************************************************************************
+************************************ SETUP **********************************
+*****************************************************************************/
 size_t max_idx = 0;
 
 int classification_flag = 0;
@@ -292,30 +247,31 @@ CircularBuffer<float, FEATURE_LEN> filt_mav_ch_03;
 CircularBuffer<float, FEATURE_LEN> filt_mav_ch_04;
 CircularBuffer<float, FEATURE_LEN> filt_mav_ch_05;
 
-void setup() 
+void compute_alpha_coeffs()
 {
-  /* ----------------------------------------------------------------------- */
-  Serial.begin( BAUD_RATE);
-  delay(10000);
-  /* ----------------------------------------------------------------------- */
-  for (int finger = 0; finger < SERVOS; finger++) moveFinger (finger, OPEN);  // Open Hand
-  delay (1000);                                            // Wait for servos
-  for (int finger = 0; finger < SERVOS; finger++) myservo[finger].detach();   // Relax servos
-  delay (1000);
-  /* ----------------------------------------------------------------------- */
   for( int i = 0; i < N_CHANS; i++)
   {
     alpha[i] = FRAME_LEN * stdevs[i] * norms[i];
     alpha[i] = 1.0 / alpha[i];
   }
-  /* ----------------------------------------------------------------------- */
+}
+
+void initialize_hand()
+{
+  for (int finger = 0; finger < SERVOS; finger++) moveFinger (finger, OPEN);
+  delay (1000);
+  for (int finger = 0; finger < SERVOS; finger++) myservo[finger].detach();
+  delay (1000);
+}
+
+void setup() 
+{
+  Serial.begin( BAUD_RATE);
+  delay(10000);
   
-//  Serial.print("Expected: ");
-//  Serial.print(0);
-//  Serial.print(" Got: ");
-//  Serial.print(env_data_ch_01.size());
-//  Serial.print(" Capacity: ");
-//  Serial.println(env_data_ch_01.capacity);
+  initialize_hand();
+  compute_alpha_coeffs();
+  
   for( int i = 0; i < FRAME_LEN; i++)
   {
     env_data_ch_01.push( analogRead( CH_01));
@@ -325,33 +281,16 @@ void setup()
     env_data_ch_05.push( analogRead( CH_05));
     delayMicroseconds(3500);
   }
-//  Serial.print("ERROR: Illegal buffer size. ");
-//  Serial.print("Expected: ");
-//  Serial.print(FRAME_LEN);
-//  Serial.print(" Got: ");
-//  Serial.print(env_data_ch_01.size());
-//  Serial.print(" Capacity: ");
-//  Serial.println(env_data_ch_01.capacity);
 }
 
-/******************************************************************************/
+
 /******************************************************************************/
 /************************************ LOOP ************************************/
 /******************************************************************************/
-/******************************************************************************/
-
-float dummy = -0.25;
-
 void loop() 
 {
-  /***************************************************************************/
-//  Serial.println("I'm alive");
-  dummy *= -1;
-//  Serial.print("clock:");
-//  Serial.print(dummy+5);
-//  Serial.print(" ");
-  
-  for( int i = 0; i < STEP_LEN; i++){
+  for( int i = 0; i < STEP_LEN; i++)
+  {
     env_data_ch_01.push( analogRead( CH_01));
     env_data_ch_02.push( analogRead( CH_02));
     env_data_ch_03.push( analogRead( CH_03));
@@ -367,7 +306,8 @@ void loop()
     delayMicroseconds(3500);
   }  
 
-  if (env_data_ch_01.size() != FRAME_LEN){
+  if (env_data_ch_01.size() != FRAME_LEN)
+  {
     Serial.print("ERROR: Illegal buffer size. ");
     Serial.print("Expected: ");
     Serial.print(FRAME_LEN);
@@ -406,10 +346,6 @@ void loop()
   mav[3] = mav_ch_04 * alpha[3];
   mav[4] = mav_ch_05 * alpha[4];
   
-//  Serial.print("mav_ch_05:");
-//  Serial.print(mav[4]);
-//  Serial.print(" ");
-
   filterData01(mav);
 
   filt_mav_ch_01.push( filteredData01[0]);
@@ -418,61 +354,12 @@ void loop()
   filt_mav_ch_04.push( filteredData01[3]);
   filt_mav_ch_05.push( filteredData01[4]);
 
-  Serial.print("filt_mav_ch_01:");
-  Serial.print(filt_mav_ch_01.last());
-  Serial.print(" ");
-  Serial.print("filt_mav_ch_03:");
-  Serial.print(filt_mav_ch_03.last());
-  Serial.print(" ");
-  Serial.print("filt_mav_ch_05:");
-  Serial.print(filt_mav_ch_05.last());
-  Serial.print(" ");
-
   float mav_sum = filteredData01[0] + filteredData01[1] + filteredData01[2] + filteredData01[3] + filteredData01[4];
   Serial.print("mav_sum:");
   Serial.println(mav_sum);
-//  Serial.print(" ");
     
   filterData02(mav_sum);
-//  Serial.print("filt_mav_sum:");
-//  Serial.print(filteredData02);
-//  Serial.print(" ");
-
-//  Serial.print("Threshold:");
-//  Serial.println(THRESHOLD);
   /***************************************************************************/
-  // for (int i = 0; i < FRAME_LEN; i++)
-  // {
-  //   for(int j = 0; j < N_CHANS; j++)
-  //   {
-  //     temp_data = analogRead(CH_01+j);
-  //     temp_data = temp_data - means[j];
-  //     temp_data = abs(temp_data);
-  //     mav[j]+= temp_data;
-  //     if( i == ( FRAME_LEN -1))
-  //     {
-  //       mav[j] *= alpha[j];
-  //     }
-  //   }
-  //   if( i < FRAME_LEN-1){
-  //     delay(4);
-  //   }
-  // }
-
-  // filterData01( mav);
-
-  // float mav_sum = 0;
-    
-  // for( int j = 0; j < N_CHANS; j++)
-  // {
-  //   mav_sum += filteredData01[j]; 
-  // }
-
-  // filterData02( mav_sum);
-
-//  float env_data[SIGNAL_LEN][N_CHANS]= { };
-//  float mav_feat[5]={0,0,0,0,0};
-
   if( filteredData02 < D_THRESHOLD) debounce_flag = 0;
   if( debounce_flag < 1)
   {
@@ -492,56 +379,6 @@ void loop()
         features[feat_idx_count] = filt_mav_ch_05 [i];
         feat_idx_count++;
       }
-
-      Serial.println(filt_mav_ch_05[0]);
-      Serial.println(filt_mav_ch_05[1]);
-      Serial.println(filt_mav_ch_05[2]);
-      Serial.println(filt_mav_ch_05[3]);
-      Serial.println(filt_mav_ch_05[4]);
-      Serial.println(filt_mav_ch_05[5]);
-      Serial.println(filt_mav_ch_05[6]);
-      Serial.println(filt_mav_ch_05[7]);
-      Serial.println(filt_mav_ch_05[8]);
-      Serial.println(filt_mav_ch_05[9]);
-      Serial.println(filt_mav_ch_05[10]);
-      
-      /************************************************************************/
-      // for ( int i = 0; i < SIGNAL_LEN; i++)
-      // {
-      //   for( int j = 0; j < N_CHANS; j++)
-      //   {
-      //     env_data[i][j] = analogRead( CH_01+j);
-      //   }
-      //   delay(4);
-      // }
-      
-      // for( int i = 0; i < FEATURE_LEN; i++)
-      // {
-      //   for ( int k = i * STEP_LEN; k < i * STEP_LEN + FRAME_LEN; k++)
-      //   {
-      //     for (int j = 0; j < N_CHANS; j++)
-      //     {
-      //       temp_data = env_data[k][j];
-      //       temp_data = temp_data - means[j];
-      //       temp_data = abs( temp_data);
-      //       mav_feat[j] += temp_data;
-      //       if(k == FRAME_LEN -1)
-      //       {
-      //         mav_feat[j] *= alpha[j];
-      //       }
-      //     }
-      //   }
-        
-      //   filterData03(mav_feat);
-        
-      //   for(int j = 0; j<N_CHANS;j++)
-      //   {
-      //     features[feat_idx_count] = filteredData03[j];
-      //     feat_idx_count++;
-      //     mav_feat[j] = 0;
-      //     filteredData03[j] = 0; 
-      //   }
-      // }
       classification_flag = 1;
       debounce_flag = 1;
     }
