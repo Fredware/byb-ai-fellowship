21bcec8cee95d533bda82f191c384ace98ae3be1
Fredware <35937021+Fredware@users.noreply.github.com>
2021-07-28 11:48:46 -0400

    Fixed Arduino CSV generator
    ard_serial_to_csv.py now outputs the feature vectors in the format specified by Edge Impulse

--- python-scripts/ard_serial_to_csv.py
+++ python-scripts/ard_serial_to_csv.py
@@ -2,25 +2,29 @@ import serial
 from struct import unpack
 
 # setup
+N_EVENTS = 5
 arduino_port = "COM10"
 baud = 115200
 
 nano_serial = serial.Serial(arduino_port, baud)
 print("Connected to Arduino port: " + arduino_port)
-csv_buff = ['timestamp,','ch0,','ch1,','ch2,','ch3,','ch4,','ch5\n']
 
 # loop
-for event in range(20):
+for event in range(N_EVENTS):
+    csv_buff = ['timestamp,','ch1,','ch2,','ch3,','ch4,','ch5\n', '0,']
     feature_table = unpack( '55f', nano_serial.read(55*4))
+        
     for count, feature in enumerate(feature_table):
         str_element = str(feature)
-        if (count + 1) % 5 == 0:
-            str_element = str_element + ','
-        else:
+        if( count + 1) % 5 == 0:
             str_element = str_element + '\n'
-        csv_buff.append(str_element)
-    # feature_table = [str(x) + ", " for x in feature_table]
+            csv_buff.append(str_element)
+            if( count != 54):
+                csv_buff.append(str((count + 1)/5) + ',')
+        else:
+            str_element = str_element + ','
+            csv_buff.append(str_element)
+    
     print("Got it!")   
     with open(f'thumb_data_{event}.csv', 'w') as file:
-        # file.writelines( feature_table)
         file.writelines(csv_buff)
