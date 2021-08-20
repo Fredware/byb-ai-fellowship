e7d67fd974a69507a438b5bd8e11a73a18bcfb94
Fredware <35937021+Fredware@users.noreply.github.com>
2021-08-20 18:56:15 -0400

    Refactoring Update
    Replaced magic number with global variables

--- python-scripts/ard_serial_to_csv.py
+++ python-scripts/ard_serial_to_csv.py
@@ -2,26 +2,34 @@ import serial
 from struct import unpack
 
 # setup
-SESSION = 3
+SESSION_ID = 3
 FINGER_NAME = 'pinky'
 
 N_EVENTS = 55
 arduino_port = "COM10"
 baud = 115200
 
-nano_serial = serial.Serial(arduino_port, baud)
-print("Connected to Arduino port: " + arduino_port)
+N_CHANS = 5
+N_FEATURES = 11
+FEATURE_BYTESIZE = 4
+
+nano_serial = serial.Serial( arduino_port, baud)
+print( "Connected to Arduino port: " + arduino_port)
 
 # loop
-for event in range(N_EVENTS):
+for event in range( N_EVENTS):
+    # Initialize buffer with column headers
     csv_buff = ['timestamp,','ch1,','ch2,','ch3,','ch4,','ch5\n', '0,']
-    feature_table = unpack( '55f', nano_serial.read(55*4))
+    # Get the features for all channels
+    feature_table = unpack( '55f', nano_serial.read( N_CHANS * N_FEATURES * FEATURE_BYTESIZE))
         
-    for count, feature in enumerate(feature_table):
+    for count, feature in enumerate( feature_table):
         str_element = str(feature)
-        if( count + 1) % 5 == 0:
+        # Add a newline at the end of a row
+        if( count + 1) % N_CHANS == 0:
             str_element = str_element + '\n'
             csv_buff.append(str_element)
+            # Add the timestamp of the next row except for the last element
             if( count != 54):
                 csv_buff.append(str((count + 1)/5) + ',')
         else:
@@ -29,5 +37,5 @@ for event in range(N_EVENTS):
             csv_buff.append(str_element)
     
     print(f'Got {event+1} out of {N_EVENTS}')   
-    with open(f'{FINGER_NAME}_data_{SESSION}_{event}.csv', 'w') as file:
+    with open(f'{FINGER_NAME}_data_{SESSION_ID}_{event}.csv', 'w') as file:
         file.writelines(csv_buff)
